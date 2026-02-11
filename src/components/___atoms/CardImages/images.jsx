9bc0d7b1538c8b98b4cff161211a16f0ac658627
Giga <gigashekelashvili1@gmail.com>
2026-02-11 18:02:31 +0400

    Feat: added mark svg

--- src/components/___atoms/CardImages/images.jsx
+++ src/components/___atoms/CardImages/images.jsx
@@ -1,10 +1,14 @@
 import "./images.css";
+import Mark from "../../../assets/images/mark.svg";
 
 function Image({ src, alt = "", className = "" }) {
   return (
     <div className="image_container">
       {" "}
       <img src={src} alt={alt} className={className} />
+      <div className="circle">
+        <img src={Mark} alt="mark" />
+      </div>
     </div>
   );
 }
